85008261cb3a1f711ceefd3d4df339031b29fd13
FoxxMD <FoxxMD@users.noreply.github.com>
2024-01-30 15:30:35 -0500

    fix: Fix punctuation transform removing non-latin letter/characters
    * Use a stricter regex to remove only common punctuation/symbols which fixes accidental removal of non-latin letter-characters
    * Provide non-alphanumeric removal as an exported transform

--- src/normalization/index.ts
+++ src/normalization/index.ts
@@ -1,6 +1,7 @@
 import {StringTransformFunc} from "../atomic.js";
 
-const PUNCTUATION_REGEX = new RegExp(/[^\w\s]|_/g);
+const PUNCTUATION_REGEX = new RegExp(/[`=(){}<>;',.~!@#$%^&*_+|:"?\-\\\[\]\/]/g);
+const NON_ALPHANUMERIC_REGEX = new RegExp(/[^\w\s]|_/g);
 const WHITESPACE_REGEX = new RegExp(/\s/g);
 const MULTI_WHITESPACE_REGEX = new RegExp(/\s{2,}/g);
 
@@ -8,6 +9,7 @@ const lowercase: StringTransformFunc = (str: string) => str.toLocaleLowerCase();
 const trim: StringTransformFunc = (str: string) => str.trim();
 const replaceUnicode: StringTransformFunc = (str: string) => str.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
 const removePunctuation: StringTransformFunc = (str: string) => str.replace(PUNCTUATION_REGEX, '');
+const removeNonAlphanumeric: StringTransformFunc = (str: string) => str.replace(NON_ALPHANUMERIC_REGEX, '');
 const removeWhitespace: StringTransformFunc = (str: string) => str.replace(WHITESPACE_REGEX, '');
 const replaceMultiWhitespace: StringTransformFunc = (str: string) => str.replace(MULTI_WHITESPACE_REGEX, ' ');
 
@@ -34,6 +36,7 @@ export {
     replaceUnicode,
     removePunctuation,
     removeWhitespace,
+    removeNonAlphanumeric,
     replaceMultiWhitespace,
     transforms,
     strDefaultTransforms
