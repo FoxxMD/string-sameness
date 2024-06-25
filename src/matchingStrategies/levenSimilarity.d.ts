239d69173bdc24185f5690ddd7ce4cd7e2052d06
FoxxMD <FoxxMD@users.noreply.github.com>
2024-06-25 09:38:50 -0400

    Remove erroneously added declaration files

--- src/matchingStrategies/levenSimilarity.d.ts
+++
@@ -1,3 +0,0 @@
-import { ComparisonStrategy, ComparisonStrategyResultObject } from "../atomic.js";
-export declare const calculateLevenSimilarity: (valA: string, valB: string) => number[];
-export declare const levenStrategy: ComparisonStrategy<ComparisonStrategyResultObject>;
